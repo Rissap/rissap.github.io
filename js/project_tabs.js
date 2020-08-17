b4864598fb00444082621cf0f0a5cff9a298bf7c
Rissap <riisa.toro@gmail.com>
2020-08-17 16:49:36 +0300

    css fixes; design changes

--- js/project_tabs.js
+++ js/project_tabs.js
@@ -2,25 +2,27 @@ const front_tab = document.querySelector('#front');
 const back_tab = document.querySelector('#back');
 const games_tab = document.querySelector('#games');
 
-const front_block = document.querySelector('#front-tab');
-const back_block = document.querySelector('#back-tab');
-const games_block = document.querySelector('#games-tab');
+const front_block = document.querySelector('#projects__front');
+const back_block = document.querySelector('#projects__back');
+const games_block = document.querySelector('#projects__games');
 
+/*invisible__element*/
+additional = "invisible__element"
 
 front_tab.addEventListener('click', () => {
-	games_block.classList.add('projects__list_hidden')
-	back_block.classList.add('projects__list_hidden')
-	front_block.classList.remove('projects__list_hidden');
+	games_block.classList.add(additional);
+	back_block.classList.add(additional);
+	front_block.classList.remove(additional);
 });
 
 back_tab.addEventListener('click', () => {
-	games_block.classList.add('projects__list_hidden')
-	front_block.classList.add('projects__list_hidden')
-	back_block.classList.remove('projects__list_hidden');
+	games_block.classList.add(additional);
+	front_block.classList.add(additional);
+	back_block.classList.remove(additional);
 });
 
 games_tab.addEventListener('click', () => {
-	back_block.classList.add('projects__list_hidden')
-	front_block.classList.add('projects__list_hidden')
-	games_block.classList.remove('projects__list_hidden');
+	back_block.classList.add(additional);
+	front_block.classList.add(additional);
+	games_block.classList.remove(additional);
 });
